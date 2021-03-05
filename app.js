f23c9f9571a93cf30f97e8f45cc602d2f220b29c
Greg <g.rutherford10@gmail.com>
2021-03-05 17:40:41 +0000

    bcreate array with title

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
 const request = require("request");
 const cheerio = require("cheerio");
 
-const cheeseArray = [];
+let cheeseList = [];
 
 request(
 	"https://cdn.adimo.co/clients/Adimo/test/index.html",
 	function (error, response, body) {
-		// console.error("error:", error); // Print the error if one occurred
+		if (error !== null) {
+			console.error(error);
+		}
 		const $ = cheerio.load(body);
 
 		$("div.item").each(function (i, elem) {
+			cheeseList[i] = {};
 			let title = $(this).find("h1").text();
-			let imageUrl = $(this).find("img").attr("src");
-			let price = $(this).find(".price").text();
-			let newItem = {
-				title,
-				imageUrl,
-				price,
-			};
-
-			cheeseArray.push(newItem);
+			cheeseList[i]["title"] = title;
 		});
 	}
 );
-
-console.log(cheeseArray);
